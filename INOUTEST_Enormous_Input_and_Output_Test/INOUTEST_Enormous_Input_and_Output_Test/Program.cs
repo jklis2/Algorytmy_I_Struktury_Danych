c3e6593e76f19c4e034e695e104cec946f3531fa
Jakub Kliś <jakubklis2201@gmail.com>
2022-11-07 19:19:41 +0100

    Koniec

---
+++ INOUTEST_Enormous_Input_and_Output_Test/INOUTEST_Enormous_Input_and_Output_Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+public class Test
+{
+    public static void Main()
+    {
+        var numberOfLines = Convert.ToInt32(Console.ReadLine());
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            var numbers = Array.ConvertAll(Console.ReadLine()?.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                           ?? Array.Empty<string>(), int.Parse);
+            builder.AppendLine($"{numbers[0] * numbers[1]}");
+        }
+        Console.WriteLine(builder);
+    }
+}
