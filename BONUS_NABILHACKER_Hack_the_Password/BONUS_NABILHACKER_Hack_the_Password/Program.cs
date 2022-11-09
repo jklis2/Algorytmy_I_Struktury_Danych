47cfb7adc9c502ba8ad09ac8bd1ad0b5d2814a47
Jakub Kliś <jakubklis2201@gmail.com>
2022-11-09 20:09:30 +0100

    Koniec

---
+++ BONUS_NABILHACKER_Hack_the_Password/BONUS_NABILHACKER_Hack_the_Password/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BONUS_NABILHACKER_Hack_the_Password
+{
+    public class Problem
+    {
+        public static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] list = new string[a];
+            for (int i = 0; i < a; i++)
+            {
+                string text = Console.ReadLine();
+
+                Stack<char> stack = new Stack<char>();
+                Stack<char> stack_2 = new Stack<char>();
+                for (int w = 0; w < text.Length; w++)
+                {
+                    switch (text[w])
+                    {
+                        case '-':
+                            if (stack.Count != 0)
+                                stack.Pop();
+                            break;
+                        case '<':
+                            if (stack.Count != 0)
+                                stack_2.Push(stack.Pop());
+                            break;
+                        case '>':
+                            if (stack_2.Count != 0)
+                                stack.Push(stack_2.Pop());
+                            break;
+                        default:
+                            stack.Push(text[w]);
+                            break;
+                    }
+                }
+                while (stack.Count != 0) stack_2.Push(stack.Pop());
+                list[i] = new String(stack_2.ToArray());
+            }
+            for (int x = 0; x < a; x++)
+                Console.WriteLine(list[x]);
+        }
+    }
+}
