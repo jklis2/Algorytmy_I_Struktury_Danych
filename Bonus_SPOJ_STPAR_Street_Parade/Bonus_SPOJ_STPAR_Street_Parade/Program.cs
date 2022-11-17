2593f8a1f773a255c01addb0735d32310f6802e7
Jakub Kliś <jakubklis2201@gmail.com>
2022-11-17 20:58:21 +0100

    Koniec

---
+++ Bonus_SPOJ_STPAR_Street_Parade/Bonus_SPOJ_STPAR_Street_Parade/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bonus_SPOJ_STPAR_Street_Parade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cnt;
+            while ((Cnt = int.Parse(Console.ReadLine())) != 0)
+            {
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                Console.WriteLine(Problem.Rozwiazanie(arr) ? "yes" : "no");
+            }
+        }
+    }
+
+    public static class Problem
+    {
+        public static bool Rozwiazanie(int[] arr)
+        {
+            var pomoc = new Stack<int>();
+            int next = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                while (pomoc.Count > 0 && pomoc.Peek() == next)
+                {
+                    pomoc.Pop();
+                    next++;
+                }
+
+                int con = arr[i];
+                if (con == next)
+                {
+                    next++;
+                }
+                else if (pomoc.Count == 0 || con < pomoc.Peek())
+                {
+                    pomoc.Push(con);
+                }
+                else return false;
+            }
+            return true;
+        }
+    }
+}
